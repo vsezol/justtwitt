5008196770cad4cc952abd4d97e1dda0137f22ec
vsezol <vsevolod.zolotov02@mail.ru>
2020-05-13 07:32:30 +0700

    'create_adaptiv_navbar'

--- src/components/UI/Navbar/Navbar.js
+++ src/components/UI/Navbar/Navbar.js
@@ -1,23 +1,46 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import Media from 'react-media'
+
 import classes from './Navbar.module.sass'
 
 const Navbar = () => {
   return (
-    <nav className={classes.Navbar}>
-      <div className={classes.LinksContainer}>
-        <NavLink to="/" className={classes.Navbar__link}>
-          <i class="fas fa-fire-alt"></i>
-        </NavLink>
-        <NavLink to="/public" className={classes.Navbar__link}>
-          <i class="fas fa-rss-square"></i>
-        </NavLink>
-        <NavLink to="/private" className={classes.Navbar__link}>
-          <i class="fas fa-lock"></i>
-        </NavLink>
+    <nav className={classes['navbar']}>
+      <div className={classes['links-container']}>
+        <Media queries={{ small: '(min-width: 576px)' }}>
+          {(matches) => (
+            <>
+              <NavLink to="/" className={classes['navbar__link']}>
+                <i className="fas fa-fire-alt"></i>
+                {matches.small && (
+                  <span className="navbar__link__alt">&nbsp;popular</span>
+                )}
+              </NavLink>
+              <NavLink to="/public" className={classes['navbar__link']}>
+                <i className="fas fa-rss-square"></i>
+                {matches.small && (
+                  <span className="navbar__link__alt">&nbsp;public</span>
+                )}
+              </NavLink>
+              <NavLink to="/private" className={classes['navbar__link']}>
+                <i className="fas fa-lock"></i>
+                {matches.small && (
+                  <span className="navbar__link__alt">&nbsp;private</span>
+                )}
+              </NavLink>
+            </>
+          )}
+        </Media>
       </div>
     </nav>
   )
 }
 
 export default Navbar
+
+// extraSmall: '(max-width: 575px)',
+// small: '(max-width: 767px)',
+// medium: '(max-width: 991px)',
+// large: '(max-width: 1199px)',
+// extraLarge: '(min-width: 1200px)',
