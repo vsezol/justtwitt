c0b52108b31ebbe899420c3b80f4a6efbc2d1d6b
vsezol <vsevolod.zolotov02@mail.ru>
2020-05-15 17:28:59 +0700

    CreateTred start development

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Layout from './hoc/Layout/Layout'
 
 import PopularTreds from './components/class/PopularTreds/PopularTreds'
 import Tred from './components/class/Tred/Tred'
+import CreateTred from './components/class/CreateTred/CreateTred'
 
 class App extends Component {
   render() {
@@ -13,6 +14,7 @@ class App extends Component {
         <Route path="/" exact component={PopularTreds} />
         <Route path="/public" exact render={() => <h1>Public treds</h1>} />
         <Route path="/public/:board/:id" component={Tred} />
+        <Route path="/public/:board/create" component={CreateTred} />
         <Route path="/private" render={() => <h1>Private treds</h1>} />
       </Switch>
     )
