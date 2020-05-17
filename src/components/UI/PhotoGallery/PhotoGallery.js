039c48639d95fbec986ceb8ad8409945b1ecfc4f
vsezol <vsevolod.zolotov02@mail.ru>
2020-05-17 08:25:13 +0700

    create modal imager

--- src/components/UI/PhotoGallery/PhotoGallery.js
+++ src/components/UI/PhotoGallery/PhotoGallery.js
@@ -1,17 +1,87 @@
-import React from 'react'
+import React, { Component } from 'react'
 import classes from './PhotoGallery.module.sass'
 import { Image } from 'react-bootstrap'
 
-const PhotoGallery = props => {
-  return (
-    <div className={classes.PhotoGallery + ' d-flex flex-wrap justify-content-center'}>
-      {props.imgs.map((imgSrc, key) => (
-        <div key={key} className={classes.PhotoGallery__Item + ' p-1'}>
-          <Image key={key} src={imgSrc} className={classes.PhotoGallery__Image} rounded />
+class PhotoGallery extends Component {
+  state = {
+    isModal: false,
+    modalImageSrc: null
+  }
+
+  onOpenImage(event) {
+    const src = event.target.src
+    this.setState(() => ({
+      isModal: true,
+      modalImageSrc: src
+    }))
+  }
+
+  onCloseImage() {
+    console.log('click')
+    this.setState(() => ({
+      isModal: false,
+      modalImageSrc: null
+    }))
+  }
+
+  renderModalImage() {
+    return (
+      <div
+        className={classes.ModalOverlay}
+        onClick={this.onCloseImage.bind(this)}
+      >
+        <div className={classes.ModalOverlay__Wrapper}>
+          <Image
+            className={classes.ModalOverlay__Wrapper__Image}
+            src={this.state.modalImageSrc}
+          />
         </div>
-      ))}
-    </div>
-  )
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div
+        className={
+          classes.PhotoGallery +
+          ' w-100 d-flex flex-wrap justify-content-center'
+        }
+      >
+        {this.state.isModal ? this.renderModalImage() : null}
+        {this.props.imgs.map((imgSrc, key) => (
+          <div key={key} className={classes.PhotoGallery__Item + ' p-1'}>
+            <Image
+              key={key}
+              src={imgSrc}
+              className={classes.PhotoGallery__Image}
+              onClick={this.onOpenImage.bind(this)}
+              rounded
+            />
+          </div>
+        ))}
+      </div>
+    )
+  }
 }
 
 export default PhotoGallery
+
+// import React from 'react'
+// import classes from './PhotoGallery.module.sass'
+// import { Image } from 'react-bootstrap'
+
+// const PhotoGallery = props => {
+
+// return (
+//   <div className={classes.PhotoGallery + ' w-100 d-flex flex-wrap justify-content-center'}>
+//     {props.imgs.map((imgSrc, key) => (
+//       <div key={key} className={classes.PhotoGallery__Item + ' p-1'}>
+//         <Image key={key} src={imgSrc} className={classes.PhotoGallery__Image} rounded />
+//       </div>
+//     ))}
+//   </div>
+// )
+// }
+
+// export default PhotoGallery
