e4b474e5ee0367f040855df600cc0b3e26245001
vsezol <vsevolod.zolotov02@mail.ru>
2020-05-19 22:08:15 +0700

    comment 1.1

--- src/components/func/CreateComment/CreateComment.js
+++ src/components/func/CreateComment/CreateComment.js
@@ -1,20 +1,47 @@
-import React from 'react'
+import React, { Component } from 'react'
 import classes from './CreateComment.module.sass'
 import TextareaAutosize from 'react-textarea-autosize'
-import { Button } from 'react-bootstrap'
 
-const CreateComment = props => {
-  return (
-    <div className={classes.CreateComment + ' w-100'}>
-      <i className={classes.CreateComment__SendButton + ' fas fa-paper-plane'} ></i>
-      <TextareaAutosize
-        placeholder='Write a comment ...'
-        className={
-          classes.CreateComment__TextArea + ' rounded-0 w-100 textarea p-2'
-        }
-      />
-    </div>
-  )
+class CreateComment extends Component {
+  state = {
+    commentText: ''
+  }
+
+  onCommentChangeHandler = event => {
+    const text = event.currentTarget.value
+    this.setState(() => ({
+      commentText: text
+    }))
+  }
+
+  onSubmitHandler = () => {
+    alert(this.state.commentText)
+  }
+
+  render() {
+    return (
+      <div
+        className={classes.CreateComment + ' w-100 d-flex align-items-center'}
+      >
+        <TextareaAutosize
+          placeholder='Write a comment ...'
+          className={
+            classes.CreateComment__TextArea +
+            ' rounded-0 border-0 w-100 textarea p-2'
+          }
+          value={this.state.commentText}
+          onChange={this.onCommentChangeHandler.bind(this)}
+        />
+        <i
+          className={
+            classes.CreateComment__SendButton +
+            ' fas fa-arrow-circle-right rounded-circle'
+          }
+          onClick={() => this.onSubmitHandler()}
+        ></i>
+      </div>
+    )
+  }
 }
 
 export default CreateComment
