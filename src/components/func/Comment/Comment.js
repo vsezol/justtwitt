c4f79871a8100a2fa32a8276926aae1c98af5a2a
vsezol <vsevolod.zolotov02@mail.ru>
2020-05-22 11:57:54 +0700

    style comments

--- src/components/func/Comment/Comment.js
+++ src/components/func/Comment/Comment.js
@@ -1,28 +1,24 @@
 import React from 'react'
-import { Media, Image } from 'react-bootstrap'
+import { Image } from 'react-bootstrap'
 import avatarImg from './avatar.svg'
 import classes from './Comment.module.sass'
 import replaceNlToBr from '../../../modules/replaceNlToBr/replaceNlToBr'
 
 const Comment = props => {
   return (
-    <div className={classes.Comment}>
-      <Media>
-        <Image
-          width={48}
-          height={48}
-          className='mr-3'
-          rounded
-          src={avatarImg}
-        />
-        <Media.Body>
-          <h5 className={classes.Comment__Id + ' m-0 p-0'}>№ {props.id}</h5>
-          <p className={classes.Comment__Text + ' m-0 p-0'}>
-            {replaceNlToBr(props.text)}
-          </p>
-        </Media.Body>
-      </Media>
-      {props.len !== props.index + 1 ? <hr className='mt-2 mb-2' /> : null}
+    <div className={classes.Comment + ' p-1'}>
+      <Image
+        width={32}
+        height={32}
+        className={classes.Comment__Avatar + ' mr-3'}
+        roundedCircle
+        src={avatarImg}
+      />
+      <div>
+        <p className={classes.Comment__Text + ' m-0 p-1 rounded'}>
+          {replaceNlToBr(props.text)}
+        </p>
+      </div>
     </div>
   )
 }
