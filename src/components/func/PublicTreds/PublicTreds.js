0bdb185fe421f83b97d5e134b7abb8346ba95c8c
vsezol <vsevolod.zolotov02@mail.ru>
2020-05-27 11:22:57 +0700

    styled nav

--- src/components/func/PublicTreds/PublicTreds.js
+++ src/components/func/PublicTreds/PublicTreds.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { Container, Row, Col } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 import axios from '../../../axios/customAxios'
+import classes from './PublicTreds.module.sass'
 
 const PublicTreds = () => {
   const [boards, setBoards] = useState(null)
@@ -16,13 +17,27 @@ const PublicTreds = () => {
 
   const renderBoards = boards =>
     Object.keys(boards).map((name, index) => (
-      <Col sm={2} key={index}>
-        <Link to={`/public/${boards[name]}`}>{name}</Link>
+      <Col xs={6} sm={4} md={3} lg={2} key={index} className='p-2'>
+        <Link
+          to={boards[name]}
+          className={classes.PublicTreds__NavLink + ' p-2 text-center rounded'}
+        >
+          {name}
+        </Link>
       </Col>
     ))
   return (
     <Container>
-      <Row>{!!boards && renderBoards(boards)}</Row>
+      <div className='pt-4'>
+        <h1 className={classes.PublicTreds__Title + ' m-0'}>Публичные треды</h1>
+        <div className={classes.PublicTreds__Logo}>
+
+        </div>
+      </div>
+
+      <div className='p-4 mb-2'>
+        <Row>{!!boards && renderBoards(boards)}</Row>
+      </div>
     </Container>
   )
 }
